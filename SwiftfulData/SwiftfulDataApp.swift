7cbce73fc0938aea2c6856aa74b1fd7ec614370c
Khondakar Afridi <afridi.khondakar@gmail.com>
2023-12-19 16:32:00 +0600

    Initial Commit

---
+++ SwiftfulData/SwiftfulDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulDataApp.swift
+//  SwiftfulData
+//
+//  Created by Khondakar Afridi on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
